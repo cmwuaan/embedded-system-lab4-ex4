5ca14891b74b6a11b3bd5c011fe0b10bd71fa91d
cmwuaan <minhquanrbpk@gmail.com>
2023-11-24 22:12:23 +0700

    update file lab 4

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define trigPin 2
+#define echoPin 3
 
-void setup() {
+byte ledPins[] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+byte pinCount;
+unsigned long duration = 0;
+int distance = 0;
+int distanceMax = 100;
+
+int getDistance();
+
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinCount = sizeof(ledPins);
+  for (int i = 0; i < pinCount; i++)
+  {
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW);
+  }
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-}
+  distance = getDistance();
+  delay(10);
+  Serial.println(distance);
+  for (int i = 0; i < pinCount; i++)
+  {
+    digitalWrite(ledPins[i], LOW);
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int ledIndex = map(distance, 1, distanceMax, 0, pinCount);
+  // Serial.println(ledIndex);
+  for (int i = ledIndex; i < pinCount; i++)
+  {
+    digitalWrite(ledPins[i], HIGH);
+  }
 }
